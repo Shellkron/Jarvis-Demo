0ad7fadd384cfdc1f0ce11abe80c999ab93a2831
Adán <adan@shellkron.com>
2017-01-12 19:00:43 +0100

    Se añaden comandos
    Se añade soporte para múltiples comandos

--- oscova-console-bot-master/Program.cs
+++ oscova-console-bot-master/Program.cs
@@ -4,6 +4,10 @@ using Syn.Bot.Siml;
 using System.IO;
 using Syn.Bot.Siml.Interfaces;
 using System.Xml.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace OscovaConsoleBot
 {
@@ -12,8 +16,11 @@ namespace OscovaConsoleBot
         [STAThread]
         static void Main(string[] args)
         {
+            var directory = Path.Combine(Directory.GetCurrentDirectory(), "WordNet");
+
             var chatbot = new SimlBot();
-            chatbot.PackageManager.LoadFromString(File.ReadAllText("Knowledge.simlpk"));
+            chatbot.PackageManager.LoadFromString(File.ReadAllText(Path.Combine(directory, "jarvis.simlpk")));
+            chatbot.Adapters.Add(new ExecuteApp());
 
             //chatbot.MainUser.ResponseReceived += (sender, eventArgs) =>
             //{
@@ -34,21 +41,18 @@ namespace OscovaConsoleBot
             {
                 var request = Console.ReadLine();
                 var evaluationResult = chatbot.Chat(request);
-                evaluationResult.BotMessage();
-                var serialized = evaluationResult.Serialize();
-
-                Console.WriteLine(serialized);
+                Console.WriteLine(evaluationResult.BotMessage);
             }
 
-            while (true)
-            {
-                var request = Console.ReadLine();
-                var evaluationResult = bot.Evaluate(request);
-                evaluationResult.Invoke();
-                var serialized = evaluationResult.Serialize();
+            //while (true)
+            //{
+            //    var request = Console.ReadLine();
+            //    var evaluationResult = bot.Evaluate(request);
+            //    evaluationResult.Invoke();
+            //    var serialized = evaluationResult.Serialize();
 
-                Console.WriteLine(serialized);
-            }
+            //    Console.WriteLine(serialized);
+            //}
         }
     }
 
@@ -67,14 +71,78 @@ namespace OscovaConsoleBot
         {
             get
             {
-                XNamespace ns = "http:\\contoso.com";
+                XNamespace ns = "http://example.com/class#adapter";
                 return ns + "ExecuteApp";
             }
         }
 
         public string Evaluate(Syn.Bot.Siml.Context context)
         {
-            throw new NotImplementedException();
+            Task.Factory.StartNew(() => 
+            {
+                var peticion = RecuperarParametros(context.Element.Value).SingleOrDefault(t => t.Item1 == "-url").Item2.Replace(" ", "+");
+                var chrome = new ProcessStartInfo(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                chrome.Arguments = string.Format("https://www.google.es/#q={0}", peticion);
+                chrome.RedirectStandardOutput = true;
+                chrome.UseShellExecute = false;
+
+                var proc = Process.Start(chrome);
+                Console.WriteLine(proc.StandardOutput.ReadToEnd());
+                proc.WaitForExit();
+            });
+
+            return string.Empty;
+        }
+
+        public List<Tuple<string, string>> RecuperarParametros(string valor)
+        {
+            var result = new List<Tuple<string, string>>();
+            string key = string.Empty;
+            string value = string.Empty;
+            var dividirXespacios = valor.Split(' ').ToList();
+
+            while (dividirXespacios.Count() > 0)
+            {
+                var elementoExtraido = dividirXespacios.FirstOrDefault();
+                dividirXespacios = dividirXespacios.Skip(1).ToList();
+
+                if (string.IsNullOrWhiteSpace(elementoExtraido))
+                    continue;
+
+                //se trata de una key
+                if (elementoExtraido.FirstOrDefault() == '-')
+                {
+                    //ya hay una key anterior
+                    if (!string.IsNullOrEmpty(key))
+                    {
+                        result.Add(new Tuple<string, string>(key, value.Trim()));
+                        key = string.Empty;
+                        value = string.Empty;
+                    }
+
+                    key = elementoExtraido.Trim();
+                }
+                //se trata de un valor
+                else
+                {
+                    value += string.Format(" {0}", elementoExtraido);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
+            {
+                result.Add(new Tuple<string, string>(key, value.Trim()));
+            }
+
+            return result;
+        }
+
+        public bool ComprobarServicio()
+        {
+            var servicio = "http://mamutpre.cinconet.local/ServicioMamut.svc";
+
+
+            return false;
         }
     }
 }
